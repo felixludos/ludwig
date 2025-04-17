6370dd3d9a7dbdb9055eacf8008bf9dff387795f
felixludos <felixludos@hotmail.com>
2025-04-17 16:38:16 +0200

    Enhance search functionality by adding GenericSearch class and updating NaiveSearch to inherit from Configurable

--- ludwig/util/search.py
+++ ludwig/util/search.py
@@ -7,14 +7,13 @@ class AbstractSearch:
 	pass
 
 
+
 T = TypeVar('T')
 
-class NaiveSearch(AbstractSearch):
+class NaiveSearch(fig.Configurable, AbstractSearch):
 	# Generic search API requiring two functions: expand and evaluate
 	# expand: T -> Iterable[T] - returns the possible next states from a given state
 	# evaluate: Iterable[T] -> Optional[str] - returns a message if the trajectory is complete, otherwise None
-
-
 
 	@classmethod
 	def search_step(cls, traj: Iterable[T], expand: Callable[[T], Iterable[T]], evaluate: Callable[[Iterable[T]], Optional[str]]) -> Iterable[T]:
@@ -29,6 +28,52 @@ class NaiveSearch(AbstractSearch):
 	def search(cls, init_state: T, expand: Callable[[T], Iterable[T]], evaluate: Callable[[Iterable[T]], Optional[str]]) -> Iterable[T]:
 		return cls.search_step((init_state,), expand, evaluate)
 	
+
+
+class GenericSearch(fig.Configurable, AbstractSearch):
+	def __init__(self, *, breadth_first: bool = True, 
+			  max_depth: Optional[int] = None, fuel: Optional[int] = None, **kwargs):
+		super().__init__(**kwargs)
+		self.breadth_first = breadth_first
+		self.max_depth = max_depth
+		self.fuel = fuel
+
+
+		self.namespace = {}
+		self.expand_fn_name = None
+		self.select_fn_name = None
+		self.extract_fn_name = None
+		self.evaluate_fn_name = None
+
+	def include_expand_items(self, items: Dict[str, Any], *, expand_fn_name: str = 'expand') -> Iterable[T]:
+		self.namespace.update(items)
+		self.expand_fn_name = expand_fn_name
+		return self
+	
+	def include_select_items(self, items: Dict[str, Any], *, select_fn_name: str = 'select') -> Iterable[T]:
+		self.namespace.update(items)
+		self.select_fn_name = select_fn_name
+		return self
+	
+	def include_extract_items(self, items: Dict[str, Any], *, extract_fn_name: str = 'extract') -> Iterable[T]:
+		self.namespace.update(items)
+		self.extract_fn_name = extract_fn_name
+		return self
+	
+	def include_evaluate_items(self, items: Dict[str, Any], *, evaluate_fn_name: str = 'evaluate') -> Iterable[T]:
+		self.namespace.update(items)
+		self.evaluate_fn_name = evaluate_fn_name
+		return self
+	
+	def include_policy_items(self, items: Dict[str, Any], *, policy_fn_name: str = 'policy') -> Iterable[T]:
+		self.namespace.update(items)
+		self.policy_fn_name = policy_fn_name
+		return self
+	
+	def include_step_items(self, items: Dict[str, Any], *, step_fn_name: str = 'step') -> Iterable[T]:
+		self.namespace.update(items)
+		self.step_fn_name = step_fn_name
+		return self
 
 
 
